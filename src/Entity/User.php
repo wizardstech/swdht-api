bed45e5687e665e1a2d99cabb69cf197db669420
Louis Harang <vundaboy@gmail.com>
2019-03-25 19:14:55 +0100

    First commit

---
+++ src/Entity/User.php
@@ -0,0 +1,87 @@
+<?php
+namespace App\Entity;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * @ORM\Table(name="users")
+ * @ORM\Entity
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @Groups({"read"})
+     */
+    private $id;
+    /**
+     * @Groups({"read", "write"})
+     * @ORM\Column(type="string", length=25, unique=true)
+     */
+    private $username;
+    /**
+     * @Groups({"write"})
+     * @ORM\Column(type="string", length=500)
+     */
+    private $password;
+    /**
+     * @Groups({"read", "write"})
+     * @ORM\Column(name="is_active", type="boolean")
+     */
+    private $isActive;
+    public function __construct($username)
+    {
+        $this->isActive = true;
+        $this->username = $username;
+    }
+    public function getUsername()
+    {
+        return $this->username;
+    }
+    public function getSalt()
+    {
+        return null;
+    }
+    public function getPassword()
+    {
+        return $this->password;
+    }
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+    public function getRoles()
+    {
+        return array('ROLE_USER');
+    }
+    public function eraseCredentials()
+    {
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getIsActive(): ?bool
+    {
+        return $this->isActive;
+    }
+
+    public function setIsActive(bool $isActive): self
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+}
